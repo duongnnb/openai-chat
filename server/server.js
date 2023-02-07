62432a9b4a2f2227f5a84918abf15bf7fcec1711
Nguyen Duong <nnbduong@gmail.com>
2023-02-07 02:50:21 +0000

    update model

--- server/server.js
+++ server/server.js
@@ -28,7 +28,30 @@ app.post('/', async (req, res) => {
     console.log('prompt', prompt)
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "code-davinci-002",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    })
+    console.log('response', response)
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    })
+  } catch (error) {
+    console.log('error', error)
+    res.status(500).send({ error })
+  }
+})
+
+app.get('/test', async (req, res) => {
+  try {
+    const prompt = "what is your name?"
+        
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
       prompt: `${prompt}`,
       temperature: 0,
       max_tokens: 3000,
